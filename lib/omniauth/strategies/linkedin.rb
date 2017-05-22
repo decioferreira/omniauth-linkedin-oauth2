77a7946e95c906f9d0ead709d1d8a994995fd7f0
Oleh Pratsko <olehpratsko@gmail.com>
2017-05-22 23:02:57 +0300

    Add callback_url option

--- lib/omniauth/strategies/linkedin.rb
+++ lib/omniauth/strategies/linkedin.rb
@@ -45,7 +45,7 @@ module OmniAuth
       end
 
       def callback_url
-        full_host + script_name + callback_path
+        options[:callback_url] || (full_host + script_name + callback_path)
       end
 
       alias :oauth2_access_token :access_token
